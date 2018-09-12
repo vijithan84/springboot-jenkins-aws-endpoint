f729ee3f96e35d8baaa96beba573d264c5511787
vijithan84 <nila.glasgow@gmail.com>
2018-09-12 19:34:09 +0100

    first commit

---
+++ src/main/groovy/com/awsjenkinscd/awsjenkinsspringboot/AwsJenkinsSpringbootApplication.groovy
@@ -0,0 +1,12 @@
+package com.awsjenkinscd.awsjenkinsspringboot
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class AwsJenkinsSpringbootApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run AwsJenkinsSpringbootApplication, args
+	}
+}
