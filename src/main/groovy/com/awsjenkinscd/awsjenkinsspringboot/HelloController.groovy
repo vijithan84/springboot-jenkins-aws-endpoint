62900ad640b6e0d6d4ec8512ca3eb0bbc6098ff9
vijithan84 <nila.glasgow@gmail.com>
2018-09-12 21:03:37 +0100

    checking ci 6

--- src/main/groovy/com/awsjenkinscd/awsjenkinsspringboot/HelloController.groovy
+++ src/main/groovy/com/awsjenkinscd/awsjenkinsspringboot/HelloController.groovy
@@ -14,6 +14,6 @@ class HelloController {
     @RequestMapping('/welcome')
     String welcome()
     {
-        return 'Welcome from  vijithan  jenkins'
+        return 'Welcome from  jenkins'
     }
 }
