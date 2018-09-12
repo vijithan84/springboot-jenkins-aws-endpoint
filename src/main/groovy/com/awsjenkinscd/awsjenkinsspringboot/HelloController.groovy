918130cb5e231dc1793efc98af499a03ebacab95
vijithan84 <nila.glasgow@gmail.com>
2018-09-12 20:51:48 +0100

    Hello Service added

---
+++ src/main/groovy/com/awsjenkinscd/awsjenkinsspringboot/HelloController.groovy
@@ -0,0 +1,14 @@
+package com.awsjenkinscd.awsjenkinsspringboot
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+
+    @RequestMapping('/hello')
+    String hello()
+    {
+        return 'Hello vijithan from jenkins'
+    }
+}
