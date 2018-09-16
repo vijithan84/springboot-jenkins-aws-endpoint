170306ade40f0826d8d42bd940ad9d8f586214b1
vijithan84 <nila.glasgow@gmail.com>
2018-09-16 11:55:48 +0100

    checking ci 1e

--- src/test/groovy/com/awsjenkinscd/awsjenkinsspringboot/HelloControllerTest.java
+++ src/test/groovy/com/awsjenkinscd/awsjenkinsspringboot/HelloControllerTest.java
@@ -1,7 +1,24 @@
 package com.awsjenkinscd.awsjenkinsspringboot;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class HelloControllerTest {
+    @Autowired
+    private WebTestClient webClient;
+
+    @Test
+    public void exampleTest() {
+        this.webClient.get().uri("/hello").exchange().expectStatus().isOk()
+                .expectBody(String.class).isEqualTo("Hello from jenkins");
+    }
 
 }
