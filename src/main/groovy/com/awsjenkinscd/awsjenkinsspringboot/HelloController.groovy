f6ccd7790abca6bf85cf57c254afac17eb506c9e
vijithan84 <nila.glasgow@gmail.com>
2018-09-20 16:13:32 +0100

    testing 2

--- src/main/groovy/com/awsjenkinscd/awsjenkinsspringboot/HelloController.groovy
+++ src/main/groovy/com/awsjenkinscd/awsjenkinsspringboot/HelloController.groovy
@@ -14,6 +14,6 @@ class HelloController {
     @RequestMapping('/welcome')
     String welcome()
     {
-        return 'Hello world fgjlkdfjgldg'
+        return 'Hello world fgjlkdfjgld dfgfhdfghkdfjg'
     }
 }
